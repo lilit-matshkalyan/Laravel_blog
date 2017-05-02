816a00ce9c4eb2b5fe81244a78d5355d83f51a4e
harout-koja <harout@megaprogramming.com>
2017-05-02 18:08:28 +0400

    Company controller update

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -210,7 +210,7 @@ class CompanyController extends Controller
             $install =  Company::where('license_key',$license_key)->first();
             $install->motherboard_key = $motherboard_key;
             $install->save();
-            return response()->json(['Message'=>'Success']);
+            return response()->json(['Message'=>'Success','company_id'=>$install->id]);
         }
         else
             return response()->json(['Error'=>'License not valid or expired']);
